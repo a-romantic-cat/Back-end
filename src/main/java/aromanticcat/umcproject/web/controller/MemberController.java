754353eb620baf1234d89bb9742bd9052cc23f85
u-genuine <u_genuine@naver.com>
2024-02-13 22:13:39 +0900

    :sparkles: Operation -> ApiOperation 어노테이션으로 변경

--- src/main/java/aromanticcat/umcproject/web/controller/MemberController.java
+++ src/main/java/aromanticcat/umcproject/web/controller/MemberController.java
@@ -5,7 +5,7 @@ import aromanticcat.umcproject.converter.MemberConverter;
 import aromanticcat.umcproject.entity.Member;
 import aromanticcat.umcproject.service.MemberService;
 import aromanticcat.umcproject.web.dto.MemberResponseDTO;
-import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -18,7 +18,7 @@ public class MemberController {
     private final MemberService service;
 
     @PostMapping("/signUp")
-    @Operation(summary = "회원가입 API", description = "회원가입 API입니다.")
+    @ApiOperation(value = "회원가입 API", notes = "회원가입 API입니다.")
     public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> signup(@RequestBody String nickname,
                                                                      @RequestParam String email) {
         try {
@@ -31,7 +31,7 @@ public class MemberController {
     }
 
     @PostMapping("/mypage/nickname")
-    @Operation(summary = "닉네임 변경 API", description = "닉네임 변경 API입니다.")
+    @ApiOperation(value = "닉네임 변경 API", notes = "닉네임 변경 API입니다.")
     public ApiResponse<MemberResponseDTO.MemberJoinResultDTO> updateNickname(@RequestBody String nickname) {
         try {
             Member user = service.updateNickname(nickname);
